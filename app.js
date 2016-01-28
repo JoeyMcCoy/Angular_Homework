bdc065789d2818af85d83b832a1a9dd7e43210f0
Joey McCoy <joey_mccoy@live.com>
2016-01-28 00:38:47 -0500

    Challenge 1.7 complete

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var app;
 var app = angular.module("gemStore", []);
 (function(){
   var gem = { name: 'Azurite', price: 2.95 };
   var app = angular.module('gemStore', []);
   app.controller('StoreController',function(){
-this.product = gem;
+this.products = gems;
   });
+   var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 }
+  ];
 })();
