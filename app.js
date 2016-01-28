cbbb150d6f76672e60d62ae22233545ec1f0f3ee
Joey McCoy <joey_mccoy@live.com>
2016-01-28 11:26:21 -0500

    Challenge 2.7 complete

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var app = angular.module("gemStore", []);
   app.controller('StoreController',function(){
 this.products = gems;
   });
+  app.controller('TabController', function(){
+  this.tab = 1;
+
+  this.setTab = function(selectedTab){
+    this.tab = selectedTab;
+  };
+
+  this.isSet = function(givenTab){
+    return this.tab === givenTab;
+  };
+});
    var gems = [{
     name: 'Azurite',
     description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
